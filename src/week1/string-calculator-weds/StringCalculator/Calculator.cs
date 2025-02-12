b451ff2f27f1aa07b15090e4ab417b2b58ab4306
Jeffry Gonzalez <Jeff@hypertheory.com>
2025-02-12 15:51:05 -0500

    started kata

---
+++ src/week1/string-calculator-weds/StringCalculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if(numbers == "1")
+        {
+            return 1;
+        }
+        if (numbers == "2")
+        {
+            return 2;
+        }
+        return 0;
+    }
+}

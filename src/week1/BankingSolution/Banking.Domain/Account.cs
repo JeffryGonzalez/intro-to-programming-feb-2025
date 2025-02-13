7f71d83d4819d300d5bd529cc81f837e4d0cc34a
Jeffry Gonzalez <Jeff@hypertheory.com>
2025-02-13 12:24:35 -0500

    for the record

--- src/week1/BankingSolution/Banking.Domain/Account.cs
+++ src/week1/BankingSolution/Banking.Domain/Account.cs
@@ -3,11 +3,11 @@
 
 namespace Banking.Domain;
 
-
+public enum AccountTypes {  Standard, Gold, Platinum};
 public class Account
 {
     private decimal _currentBalance = 5000;
-
+    public AccountTypes AccountType = AccountTypes.Standard; // zero mean standard account, 1 means it is a Gold account, 2 Plant
     // Queries (methods where we ask for stuff)
     public decimal GetBalance()
     { 
@@ -15,8 +15,13 @@ public class Account
     }
     public void Deposit(decimal amountToDeposit)
     {
+        var bonus = 0M;
+        if (AccountType == AccountTypes.Gold)
+        {
+            bonus = amountToDeposit * .20M;
+        }
         CheckTransactionAmount(amountToDeposit);
-        _currentBalance += amountToDeposit;
+        _currentBalance += amountToDeposit + bonus;
     }
 
     // Commands - telling our account to do some work.
--- src/week1/BankingSolution/Banking.Tests/GoldAccounts/GetBonusOnDeposits.cs
+++ src/week1/BankingSolution/Banking.Tests/GoldAccounts/GetBonusOnDeposits.cs
@@ -8,17 +8,18 @@ public class GetBonusOnDeposits
     [Fact]
     public void GetBonus()
     {
-        // Given
+        // Given - Arrange - "Establishing the context for this test"
         var account = new Account();
         var openingBalance = account.GetBalance();
         var amountToDeposit = 100M;
         var expectedBonus = 20M;
         var expectedNewBalance = openingBalance + amountToDeposit + expectedBonus;
+        account.AccountType = AccountTypes.Gold;
 
-        // When
+        // When - Act
         account.Deposit(amountToDeposit);
 
-        // Then
+        // Then - Assert
         Assert.Equal(expectedNewBalance, account.GetBalance());
             
     }

ec011f01446f64253aba1744ed371ebcf2d45c8c
Jeffry Gonzalez <Jeff@hypertheory.com>
2025-02-14 11:54:19 -0500

    before break

---
+++ src/week1/BankingSolution/Banking.Domain/AccountTransactionAmount.cs
@@ -0,0 +1,41 @@
+
+using System.Reflection.Metadata.Ecma335;
+
+namespace Banking.Domain;
+
+public record struct AccountTransactionAmount
+{
+  private readonly decimal _amount;
+
+  //public decimal GetValue()
+  //{
+  //  return _amount;
+  //}
+  public decimal Value {
+    get
+    {
+      return _amount; 
+    }
+    
+  }
+ 
+
+  public AccountTransactionAmount(decimal value)
+  {
+    if (value < 0)
+    {
+      throw new AccountNegativeTransactionAmountException();
+    }
+    if (value > 10_000)
+    {
+      throw new AccountTranactionAmountBeyondLimits();
+    }
+    _amount = value;
+  }
+
+  public static implicit operator Decimal(AccountTransactionAmount a) => a.Value;
+
+  public static implicit operator AccountTransactionAmount(decimal value) => new(value);
+  
+
+}

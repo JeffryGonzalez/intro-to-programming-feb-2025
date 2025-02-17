3969b1f9025b45c1985e0463aa88248d11aefe5d
Jeffry Gonzalez <Jeff@hypertheory.com>
2025-02-17 13:46:03 -0500

    banking withdrawal

--- src/week2/frontend/src/app/banking/components/withdrawal.component.ts
+++ src/week2/frontend/src/app/banking/components/withdrawal.component.ts
@@ -1,10 +1,45 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  effect,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BankService } from '../services/bank.service';
 
 @Component({
   selector: 'app-banking-withdrawal',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ` <p>Make a Withdrawal</p> `,
+  imports: [ReactiveFormsModule],
+  template: ` <form [formGroup]="form" (ngSubmit)="doWithdrawal()">
+    <div class="form-control">
+      <label for="amount" class="label"
+        >Amount of Withdrawal
+        <input
+          formControlName="amount"
+          class="input input-bordered input-md"
+          type="number"
+          id="amount"
+        />
+      </label>
+      <button type="submit" class="btn btn-primary">Make Deposit</button>
+    </div>
+  </form>`,
   styles: ``,
 })
-export class WithdrawalComponent {}
+export class WithdrawalComponent {
+  service = inject(BankService);
+  form = new FormGroup({
+    amount: new FormControl<number>(0, { nonNullable: true }),
+  });
+
+  doWithdrawal() {
+    // do the deposit here.
+    this.service.withdraw(this.form.controls.amount.value);
+  }
+}

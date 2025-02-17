b384da7dc08f291dde89f1f6645cc3565601ef2b
Jeffry Gonzalez <Jeff@hypertheory.com>
2025-02-17 11:41:00 -0500

    navigation started

--- src/week2/frontend/src/app/app.component.ts
+++ src/week2/frontend/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-
-import { WelcomeComponent } from './components/welcome.component';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   template: `
+    <app-nav-bar />
     <main class="container mx-auto">
-      <app-welcome />
+      <router-outlet />
     </main>
   `,
   styles: [],
-  imports: [WelcomeComponent],
+  imports: [RouterOutlet, NavBarComponent],
 })
 export class AppComponent {}

4fd84dd835ef57233c08c5dc62dbefe0718f0ff0
Jeffry Gonzalez <Jeff@hypertheory.com>
2025-02-18 16:15:24 -0500

    done

--- src/week2/frontend/src/app/resources/services/tagmaker.ts
+++ src/week2/frontend/src/app/resources/services/tagmaker.ts
@@ -1,9 +1,13 @@
 export function tagMaker(tagList: string): string[] {
-  const parts = tagList
+  // split the string into an array with, get rid of empty strings, no duplicates, converted to lower case
+  return [];
+}
+
+/* const parts = tagList
     .split(' ')
     .filter((t) => t !== '')
     .map((t) => t.trim())
     .map((t) => t.toLowerCase());
 
   return Array.from(new Set(parts));
-}
+*/
